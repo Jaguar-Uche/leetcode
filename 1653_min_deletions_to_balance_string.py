7443fb74e5fee474445fd93a782ed2386204177b
Alex <alexuduma50@gmail.com>
2026-02-07 22:24:02 +0100

    Day 7

---
+++ 1653_min_deletions_to_balance_string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        b_count = 0
+        deletions = 0
+
+        for ch in s:
+            if ch == 'a':
+                deletions = min(deletions + 1, b_count)
+            else:
+                b_count += 1
+
+        return deletions
+
+
+s = Solution()
+print(s.minimumDeletions("ababaaaabbbbbaaababbbbbbaaabbaababbabbbbaabbbbaabbabbabaabbbababaa"))
