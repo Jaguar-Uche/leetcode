7e04c1414f9261b64b922bff1c98144c795836a3
Alex <alexuduma50@gmail.com>
2026-02-23 17:07:37 +0100

    Day 23

--- 48_rotate_image.py
+++ 48_rotate_image.py
@@ -4,6 +4,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
         n = len(matrix)
+        arr = []
+        for i in range(n):
+            temp = []
+            for j in range(n):
+                temp.append(matrix[j][i])
+            temp.reverse()
+            arr.append(temp)
+        for i in range(n):
+            matrix[i] = arr[i]
+        # Just to show us at the end
+        print(matrix)
 
+
+
+solution = Solution()
+solution.rotate([[1,2,3],[4,5,6],[7,8,9]])
