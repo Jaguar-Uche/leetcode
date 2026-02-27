4d53b945f5701e44aca6842d22d5812499db502a
Alex <alexuduma50@gmail.com>
2026-02-27 09:17:14 +0100

    Day 27

--- 1404_number_of_steps_to_reduce_number_to_one.py
+++ 1404_number_of_steps_to_reduce_number_to_one.py
@@ -5,7 +5,16 @@ class Solution:
         while num != 1:
             if num % 2 == 0:
                 num //= 2
+                steps += 1
             else:
-                num +=1
-            steps += 1
+                num = (num + 1) // 2
+                steps +=2
         return steps
+
+class Soln:
+    def numSteps(self, s: str) -> int:
+        carry = 0
+        for i in range(len(s)-1, -1, -1 ):
+            pass
+solution = Soln()
+print(solution.numSteps('11'))
--- 51_n_queens.py
+++ 51_n_queens.py
@@ -1,3 +1,37 @@
 from typing import List
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+    def solveNQueens(self, n: int)-> List[List[int]]:
+        result = []
+        board = [["."] * n for _ in range(n)]
+
+        cols = set()
+        diag1 = set()  # row - col
+        diag2 = set()  # row + col
+
+        def backtrack(row):
+            if row == n:
+                result.append(["".join(r) for r in board])
+                return
+
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+
+                # place queen
+                board[row][col] = "Q"
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1)
+
+                # remove queen (backtrack)
+                board[row][col] = "."
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0)
+        return result
+solution = Solution()
+print(solution.solveNQueens(4))
