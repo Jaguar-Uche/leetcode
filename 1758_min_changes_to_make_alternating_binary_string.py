65c2f4b65e8ee730d18e0c3bf92e30a07dc9f8c2
Alex <alexuduma50@gmail.com>
2026-03-07 22:25:27 +0100

    Day 7

--- 1758_min_changes_to_make_alternating_binary_string.py
+++ 1758_min_changes_to_make_alternating_binary_string.py
@@ -1,43 +1,9 @@
-def changes_one(s):
-    arr = list(s)
-    n = len(arr)
-    moves = 0
-    if arr[0] == '0':
-        moves += 1
-        arr[0] = '1'
-    for i in range(1, n):
-        if arr[i] == arr[i - 1]:
-            if arr[i] == '0':
-                arr[i] = '1'
-            else:
-                arr[i] = '0'
-
-            moves += 1
-    return moves
-
-def changes_zero(s):
-    arr = list(s)
-    n = len(arr)
-    moves = 0
-    if arr[0] == '1':
-        moves += 1
-        arr[0] = '0'
-    for i in range(1, n):
-        if arr[i] == arr[i - 1]:
-            if arr[i] == '0':
-                arr[i] = '1'
-            else:
-                arr[i] = '0'
-
-            moves += 1
-    return moves
 class Solution:
     def minOperations(self, s: str) -> int:
-        n = len(s)
-        if n == 1:
-            return 0
-        s,t = changes_zero(s), changes_one(s)
-        return min(s,t)
+        diff = 0
 
-solution = Solution()
-print(solution.minOperations("1111"))
+        for i in range(len(s)):
+            if s[i] != ("1" if i % 2 == 0 else '0'):
+                diff += 1
+
+        return min(diff, len(s) - diff)
