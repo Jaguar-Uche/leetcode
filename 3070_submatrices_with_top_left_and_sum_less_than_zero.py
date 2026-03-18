c1dab2bb687663fb1fbe090c1ad57f7d44647c50
Alex <alexuduma50@gmail.com>
2026-03-18 20:31:55 +0100

    Day 18

---
+++ 3070_submatrices_with_top_left_and_sum_less_than_zero.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+
+        # Build prefix sum in-place
+        for i in range(m):
+            for j in range(n):
+                if i > 0:
+                    grid[i][j] += grid[i - 1][j]
+                if j > 0:
+                    grid[i][j] += grid[i][j - 1]
+                if i > 0 and j > 0:
+                    grid[i][j] -= grid[i - 1][j - 1]
+
+        # Count valid submatrices
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] <= k:
+                    count += 1
+
+        return count
+
+solution = Solution()
+print(solution.countSubmatrices([[1,1,1],[1,1,1],[1,1,1]], 3))
