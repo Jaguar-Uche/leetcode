f02fee31e54b31ee48cba7b24515c25e4a468bec
Alex <alexuduma50@gmail.com>
2026-06-03 22:17:07 +0100

    Day 3

---
+++ 3635_earliest_finish_time_for_land_and_water_rides_ii.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        # Earliest possible completion of any land ride
+        min_land_finish = min(start + duration for start, duration in zip(landStartTime, landDuration))
+
+        # Best Land -> Water
+        best_land_water = min(max(min_land_finish, start) + duration for start, duration in zip(waterStartTime, waterDuration))
+
+        # Earliest possible completion of any water ride
+        min_water_finish = min(start + duration for start, duration in zip(waterStartTime, waterDuration))
+
+        # Best Water -> Land
+        best_water_land = min(max(min_water_finish, start) + duration for start, duration in zip(landStartTime, landDuration))
+
+        return min(best_land_water, best_water_land)
