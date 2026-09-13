fc0ece83d990cd3167c9661f33b419ff66b89d14
Alex <alexuduma50@gmail.com>
2026-09-13 16:05:12 +0100

    final solution of day 13

--- 835_image_overlap.py
+++ 835_image_overlap.py
@@ -3,24 +3,26 @@ from typing import List
 # Then using the 1s from img1, find the displacement from the other one, and find the one that gives you max displacement
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        img1_arr = []
-        img2_arr = []
-        hash = {}
+        img1_ones = []
+        img2_ones = []
+        shifts = {}
+
         n = len(img1)
+
         for i in range(n):
             for j in range(n):
                 if img1[i][j] == 1:
-                    img1_arr.append((i,j))
+                    img1_ones.append((i, j))
+
                 if img2[i][j] == 1:
-                    img2_arr.append((i,j))
-        # print(img1_arr)
-        # print(img2_arr)
-        for (a,b) in img1_arr:
-            for (c,d) in img2_arr:
-                e,f = c-a, d-b
-                hash[(e,f)] = hash.get((e,f), 0) + 1
-        # print(hash)
-        return max(hash.values())
+                    img2_ones.append((i, j))
+
+        for r1, c1 in img1_ones:
+            for r2, c2 in img2_ones:
+                shift = (r2 - r1, c2 - c1)
+                shifts[shift] = shifts.get(shift, 0) + 1
+
+        return max(shifts.values(), default=0)
 
 sol = Solution()
 print(sol.largestOverlap(img1 = [[1,1,0],[0,1,0],[0,1,0]], img2 = [[0,0,0],[0,1,1],[0,0,1]]))
