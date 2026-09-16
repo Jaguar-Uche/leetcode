dd5023f907e87702a59008b66c4f17504eddfd4e
Alex <alexuduma50@gmail.com>
2026-09-16 13:38:52 +0100

    Best solution for the day

--- 1621_Number_Of_Sets_Of_K_Non_Overlapping_Line_Segments.py
+++ 1621_Number_Of_Sets_Of_K_Non_Overlapping_Line_Segments.py
@@ -1,28 +1,16 @@
-from functools import lru_cache
-
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
         MOD = 10**9 + 7
-        @lru_cache(None)
-        def dp(start, remaining):
-            if remaining == 0:
-                return 1
 
-            if start >= n - 1:
-                return 0
+        # dp[j] = C(current, j)
+        dp = [0] * (2 * k + 1)
+        dp[0] = 1
 
-            total = 0
+        for x in range(1, n + k):
+            for j in range(min(x, 2 * k), 0, -1):
+                dp[j] = (dp[j] + dp[j - 1]) % MOD
 
-            # Choose the start of the next segment
-            for left in range(start, n - 1):
+        return dp[2 * k]
 
-                # Choose the end of that segment
-                for right in range(left + 1, n):
-                    total += dp(right, remaining - 1)
-                    total %= MOD
-
-            return total
-
-        return dp(0, k)
 sol = Solution()
 print(sol.numberOfSets(4,2))
